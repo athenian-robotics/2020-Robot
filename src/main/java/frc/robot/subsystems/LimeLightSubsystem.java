c0a4e43bf5c6454cacaf9697a462d8746593b4ee
pambrose <pambrose@mac.com>
2020-01-11 20:11:01 -0800

    Add LimeLightSubsystem

--- src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -14,7 +14,7 @@ public class LimeLightSubsystem extends SubsystemBase {
     }
 
     public void periodic() {
-        final NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
+        final NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight-2");
 
         final boolean tv = limelight.getEntry("tv").getBoolean(false);
         SmartDashboard.putBoolean("tv", tv);
