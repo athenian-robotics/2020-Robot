5da54239c6f25f1fd136717c897e386544028b96
Tarun <tarunchichili@gmail.com>
2020-01-14 19:23:57 -0800

    move code to initialize and add stop code

--- src/main/java/frc/robot/commands/GearBoxTest.java
+++ src/main/java/frc/robot/commands/GearBoxTest.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -28,17 +29,20 @@ public class GearBoxTest extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        drivetrain.tankDrive(0,0);
+        Timer.delay(0.1);
+        drivetrain.tankDrive(leftdirection,rightdirection);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        drivetrain.tankDrive(leftdirection,rightdirection);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        drivetrain.tankDrive(0,0);
     }
 
     // Returns true when the command should end.
