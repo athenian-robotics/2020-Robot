3b951767a05fd3c0e1721bba84761850fe76c86c
dorian <dekolis1218@gmail.com>
2020-01-20 13:09:40 -0800

    Create FlatShooterSubsystem

---
+++ src/main/java/frc/robot/subsystems/FlatShooterSubsystem.java
@@ -0,0 +1,28 @@
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FlatShooterSubsystem extends SubsystemBase {
+
+    public void FlatShooterSubsystem() {
+
+        //final motor_placeholder shooterMotor;
+        //final pneumatic_placeholder shooterPiston;
+
+    }
+
+    public void shootBalls(boolean enabled) {
+        if (enabled) {
+            //turnOnMotor();
+        } else {
+            //turnOffMotor();
+        }
+
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
