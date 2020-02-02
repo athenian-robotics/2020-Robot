7e554fd1687203f2226c3511e3ca0b0f11d46fef
JTkeio <jacobtkeio@gmail.com>
2020-02-02 15:37:37 -0800

    add CenterToGoal

---
+++ src/main/java/frc/robot/commands/CenterToGoal.java
@@ -0,0 +1,17 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.LimeLightSubsystem;
+
+
+public class CenterToGoal extends SequentialCommandGroup {
+
+    public CenterToGoal(LimeLightSubsystem limelight, DrivetrainSubsystem drivetrain) {
+        limelight.grabNetworkTable().getEntry("pipeline").setNumber(1);
+        double[] list = limelight.grabValues();
+        double angleHalfToGoal = Math.atan((list[6] / 2) / list[5]);
+
+        addCommands(new AutoTurnAngle(drivetrain, -angleHalfToGoal), new AutoDriveForwardDistance(drivetrain, Math.sin((angleHalfToGoal * list[6]) / 36)), new AutoTurnAngle(drivetrain, angleHalfToGoal));
+    }
+}
