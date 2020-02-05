03bb60678d6a69c8b528444d55b876487bc8a90a
pambrose <pambrose@mac.com>
2020-02-04 16:41:04 -0800

    Add class for faking data

---
+++ src/main/java/frc/robot/shuffleboard/CountUpDownGenerator.java
@@ -0,0 +1,76 @@
+package frc.robot.shuffleboard;
+
+import java.io.Closeable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CountUpDownGenerator implements Closeable {
+
+    private final AtomicInteger currValue = new AtomicInteger();
+    private final AtomicBoolean finished = new AtomicBoolean(false);
+
+    private final int start;
+    private final int stop;
+    private final int freqMillis;
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public CountUpDownGenerator(int start, int stop, int freqMillis) {
+        this.start = start;
+        this.stop = stop;
+        this.freqMillis = freqMillis;
+
+        this.executor.submit(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        while (!finished.get()) {
+                            for (int i = start; i < stop; i++) {
+                                currValue.set(i);
+                                if (finished.get())
+                                    break;
+                                pause(freqMillis);
+                            }
+
+                            for (int i = stop; i > start; i--) {
+                                currValue.set(i);
+                                if (finished.get())
+                                    break;
+                                pause(freqMillis);
+                            }
+                        }
+                    }
+                });
+    }
+
+    private static void pause(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            // Ignore
+        }
+    }
+
+    public static void main(String[] args) {
+        final CountUpDownGenerator generator = new CountUpDownGenerator(0, 10, 500);
+
+        for (int i = 0; i < 25; i++) {
+            System.out.println(generator.getValue());
+            pause(500);
+        }
+
+        generator.close();
+    }
+
+    public int getValue() {
+        return currValue.intValue();
+    }
+
+    @Override
+    public void close() {
+        executor.shutdownNow();
+        finished.set(true);
+    }
+}
