64d899a4292f245ab4732270abc733b4b7dec075
kylepoage <22kpoage@athenian.org>
2020-02-04 17:19:39 -0800

    Display Test data to shuffleboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,8 +8,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.shuffleboard.CountUpDownGenerator;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -21,6 +23,9 @@ public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     private RobotContainer m_robotContainer;
+
+    private CountUpDownGenerator generator = new CountUpDownGenerator(0, 10, 500);
+
 
     /**
      * This function is run when the robot is first started up and should be used for any
@@ -47,6 +52,7 @@ public class Robot extends TimedRobot {
         // and running subsystem periodic() methods.  This must be called from the robot's periodic
         // block in order for anything in the Command-based framework to work.
         CommandScheduler.getInstance().run();
+        SmartDashboard.putNumber("Test Data: ", generator.getValue());
     }
 
     /**
