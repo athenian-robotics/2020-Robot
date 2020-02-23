1282d83d902c9b5be3227443bbd2754ec318a6aa
Tarun <tarunchichili@gmail.com>
2020-02-23 14:44:23 -0800

    fix color wheel

--- src/main/java/frc/robot/commands/color_wheel/ColorWheelTest.java
+++ src/main/java/frc/robot/commands/color_wheel/ColorWheelTest.java
@@ -45,6 +45,6 @@ public class ColorWheelTest extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return true;
+        return false;
     }
 }
