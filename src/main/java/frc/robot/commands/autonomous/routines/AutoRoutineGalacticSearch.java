51a5acd9c7f7e98281f7289a3bf021f7f66c6306
JTkeio <jacobtkeio@gmail.com>
2021-02-21 01:30:12 -0800

    AutoRoutineGalacticSearch is now an instant command and self-replicates to add, effectively, looping functionality within the command/callback structure. Add 3 TODOs to show what needs to be tested to make this command work.

--- src/main/java/frc/robot/commands/autonomous/routines/AutoRoutineGalacticSearch.java
+++ src/main/java/frc/robot/commands/autonomous/routines/AutoRoutineGalacticSearch.java
@@ -13,13 +13,15 @@ public class AutoRoutineGalacticSearch extends CommandBase {
     private final IntakeSubsystem intake;
     private final LimeLightSubsystem limelight;
 
-    private int ballCount = 0;
-    private double areaWeight = 1; //@TODO determine what this weight should be so that the robot always gets the balls -- the higher this is, the farther the robot will go
+    private int ballCount;
+    private final double areaWeight = 1; //TODO determine what this weight should be so that the robot always gets the balls -- the higher this is, the farther the robot will go (and it might need to be quadratic or cubic)
+    private final int tolerance = 1; //TODO determine how much farther than 30 meters we should go to clear the field
 
-    public AutoRoutineGalacticSearch(DrivetrainSubsystem drivetrain, IntakeSubsystem intake, LimeLightSubsystem limelight) {
+    public AutoRoutineGalacticSearch(DrivetrainSubsystem drivetrain, IntakeSubsystem intake, LimeLightSubsystem limelight, int ballCount) {
         this.drivetrain = drivetrain;
         this.intake = intake;
         this.limelight = limelight;
+        this.ballCount = ballCount;
         addRequirements(this.drivetrain, this.intake, this.limelight);
     }
 
@@ -32,28 +34,24 @@ public class AutoRoutineGalacticSearch extends CommandBase {
 
     @Override
     public void execute() {
-        double[] values = limelight.grabValues();
-        if (values[0] == 1) {
-            new TurnToBall(limelight, drivetrain).schedule();
-            new AutoForwardDistance(drivetrain, values[1]*areaWeight).schedule();
-            ballCount++;
+        if (ballCount > 0) {
+            double[] values = limelight.grabValues();
+            if (values[0] == 1) {
+                new TurnToBall(limelight, drivetrain).schedule();
+                new AutoForwardDistance(drivetrain, values[1] * areaWeight).schedule();
+                new AutoRoutineGalacticSearch(drivetrain, intake, limelight, ballCount-1).schedule();        //We got a ball! Look for more.
+            } else {
+                new AutoAngleTurn(drivetrain, -45).schedule();
+                new AutoRoutineGalacticSearch(drivetrain, intake, limelight, ballCount).schedule();                    //No ball in sight, turn around and look again.
+            }
         } else {
-            new AutoAngleTurn(drivetrain, -45).schedule();
+            limelight.grabNetworkTable().getEntry("pipeline").setNumber(0);                                       //We got all the balls, turn everything off and run for the exit. :p
+            new AutoIntakeOff(intake).schedule();
+            new AutoAngleTurn(drivetrain, -drivetrain.getGyroAngle()).schedule();
+            new AutoForwardDistance(drivetrain, 30-drivetrain.getOdometryPosX()+tolerance).schedule();    //TODO determine if this should be PosX or Pos Y for which way the robot is starting
         }
     }
 
-
     @Override
-    public boolean isFinished() {
-        return ballCount > 2;
-    }
-
-
-    @Override
-    public void end(boolean interrupted) {
-        limelight.grabNetworkTable().getEntry("pipeline").setNumber(0);
-        new AutoIntakeOff(intake).schedule();
-        new AutoAngleTurn(drivetrain, -drivetrain.getGyroAngle()).schedule();
-        new AutoForwardDistance(drivetrain, 4).schedule();
-    }
+    public boolean isFinished() { return true; }    //This command ends instantly, but lives on in new instances until we get all of the balls we need
 }
