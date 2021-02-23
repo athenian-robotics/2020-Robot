7514d9d7bb271ff7557d8d2f78666d4d25fe8f8c
JTkeio <jacobtkeio@gmail.com>
2021-02-23 14:10:27 -0800

    remove TODO for determining getPosX vs getPosY -- it was getPosX :)

--- src/main/java/frc/robot/commands/autonomous/routines/AutoRoutineGalacticSearch.java
+++ src/main/java/frc/robot/commands/autonomous/routines/AutoRoutineGalacticSearch.java
@@ -15,7 +15,7 @@ public class AutoRoutineGalacticSearch extends CommandBase {
 
     private int ballCount;
     private final double areaWeight = 1; //TODO determine what this weight should be so that the robot always gets the balls -- the higher this is, the farther the robot will go
-    private final int tolerance = 1; //TODO determine how much farther than 30 meters we should go to clear the field
+    private final int tolerance = 1; //TODO determine how much farther than 9 meters we should go to clear the field
 
     public AutoRoutineGalacticSearch(DrivetrainSubsystem drivetrain, IntakeSubsystem intake, LimeLightSubsystem limelight, int ballCount) {
         this.drivetrain = drivetrain;
@@ -38,7 +38,7 @@ public class AutoRoutineGalacticSearch extends CommandBase {
             double[] values = limelight.grabValues();
             if (values[0] == 1) {
                 new TurnToBall(limelight, drivetrain).schedule();
-                new AutoForwardDistance(drivetrain, areaWeight/Math.sqrt(values[1])).schedule();        //Math.sqrt() is necessary because the robot's distance from the ball, how far we want to drive, is proportional to its radius, which is the square root of the area, or values[1]
+                new AutoForwardDistance(drivetrain, areaWeight/Math.sqrt(values[1])).schedule();          //Math.sqrt() is necessary because the robot's distance from the ball, how far we want to drive, is proportional to its radius, which is the square root of the area, or values[1]
                 new AutoRoutineGalacticSearch(drivetrain, intake, limelight, ballCount-1).schedule();        //We got a ball! Look for more.
             } else {
                 new AutoAngleTurn(drivetrain, -45).schedule();
@@ -48,7 +48,7 @@ public class AutoRoutineGalacticSearch extends CommandBase {
             limelight.grabNetworkTable().getEntry("pipeline").setNumber(0);                                       //We got all the balls, turn everything off and run for the exit. :p
             new AutoIntakeOff(intake).schedule();
             new AutoAngleTurn(drivetrain, -drivetrain.getGyroAngle()).schedule();
-            new AutoForwardDistance(drivetrain, 30-drivetrain.getOdometryPosX()+tolerance).schedule();    //TODO determine if this should be PosX or Pos Y for which way the robot is starting
+            new AutoForwardDistance(drivetrain, 9-drivetrain.getOdometryPosX()+tolerance).schedule();
         }
     }
 
